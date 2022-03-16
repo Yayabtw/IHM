841f9a490de32945a19c40ecb4a2fc41ea899888
Yanis.M <yanis.merabet@ens.uvsq.fr>
2022-03-16 11:33:39 +0100

    Finalisation de la classe PremiereScene

--- src/main/java/premiereVue/PremiereScene.java
+++ src/main/java/premiereVue/PremiereScene.java
@@ -1,13 +1,19 @@
 package premiereVue;
+import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-public class PremiereScene {
+
+public class PremiereScene extends Application {
     public void start(Stage stage){
         VBox root = new VBox();
         Scene scene = new Scene(root,300,80);
         stage.setScene(scene);
-        stage.setTitle("Hello JavaFX");
+        stage.setTitle("Salut Dorian");
         stage.show();
     }
+    public static void main(String[] args) {
+        Application.launch();
+    }
+
 }
