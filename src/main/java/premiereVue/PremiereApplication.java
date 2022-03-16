2cf85b491aa0c1ece5e52aa69e1f23bf401d5bdc
Yanis.M <yanis.merabet@ens.uvsq.fr>
2022-03-16 11:05:11 +0100

    Ajout de la classe PremiereApplication

---
+++ src/main/java/premiereVue/PremiereApplication.java
@@ -0,0 +1,20 @@
+package premiereVue;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PremiereApplication extends Application {
+
+    public void start(Stage stage)   {
+        stage.setTitle("Hello JavaFX");
+        stage.setWidth(400);
+        stage.setHeight(150);
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch();
+    }
+
+}
