87d95c98f9c9821f6f3b8a35d527172aa5b2d039
Yanis.M <yanis.merabet@ens.uvsq.fr>
2022-03-16 11:34:02 +0100

    Ajout de la classe PremiersElementsGraphiques

---
+++ src/main/java/premiereVue/PremiersElementsGraphiques.java
@@ -0,0 +1,24 @@
+package premiereVue;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class PremiersElementsGraphiques {
+    public void start(Stage stage){
+        VBox root = new VBox(10);
+        Label labelHello = new Label("Hello");
+        root.getChildren().add(labelHello);
+        Label labelHelloBis = new Label("Hello JavaFX");
+        root.getChildren().add(labelHelloBis);
+        Scene scene = new Scene(root,300,80);
+        stage.setScene(scene);
+        stage.setTitle("Salut Dorian");
+        stage.show();
+    }
+    public static void main(String[] args) {
+        Application.launch();
+    }
+}
