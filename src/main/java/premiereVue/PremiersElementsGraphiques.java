6f1078b66666115f2da4ae4b7adefd3350f1f746
Yanis.M <yanis.merabet@ens.uvsq.fr>
2022-03-23 10:46:00 +0100

    Création du fichier premiersStyles.css

--- src/main/java/premiereVue/PremiersElementsGraphiques.java
+++ src/main/java/premiereVue/PremiersElementsGraphiques.java
@@ -6,14 +6,20 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class PremiersElementsGraphiques extends Application {
     public void start(Stage stage){
         VBox root = new VBox(10);
+
         Label labelHello = new Label("Hello");
-        root.getChildren().add(labelHello);
         Label labelHelloBis = new Label("Hello JavaFX");
-        root.getChildren().add(labelHelloBis);
+        root.getChildren().addAll(labelHello, labelHelloBis);
+
         Scene scene = new Scene(root,300,80);
+        File css = new File("css" + File.separator + "premiersStyles.css");
+        scene.getStylesheets().add(css.toURI().toString());
+
         stage.setScene(scene);
         stage.setTitle("PremiersElementsGraphiques");
         stage.show();
